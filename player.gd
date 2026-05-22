e60c710dbe848dddb08f920e5987a40a386281c3
etper <piotr.brzezinski9@gmail.com>
2026-05-22 21:24:41 +0200

    healing v1

--- player.gd
+++ player.gd
@@ -11,6 +11,23 @@ extends CharacterBody3D
 @onready var camera_pivot = $CameraPivot
 @onready var camera = $CameraPivot/Camera3D
 
+@export var heal_time := 8.0
+
+@export var is_dummy := false
+
+var heal_progress := 0.0
+var is_being_healed := false
+
+var nearby_player = null
+
+enum HealthState {
+	HEALTHY,
+	INJURED,
+	DYING
+}
+
+var health_state = HealthState.HEALTHY
+
 var current_speed := 0.0
 var is_crouching := false
 
@@ -18,6 +35,9 @@ var pitch := 0.0
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if is_dummy:
+		health_state = HealthState.INJURED
 
 func _unhandled_input(event):
 
@@ -34,6 +54,15 @@ func _unhandled_input(event):
 		camera_pivot.rotation_degrees.x = pitch
 
 func _physics_process(delta):
+
+	if is_dummy:
+		return
+
+	if Input.is_action_just_pressed("ui_accept"):
+		health_state = HealthState.INJURED
+		print("INJURED")
+
+	handle_healing(delta)
 
 	# gravity
 	if not is_on_floor():
@@ -108,3 +137,38 @@ func uncrouch():
 		1.6,
 		0.2
 	)
+
+func _on_heal_area_body_entered(body):
+
+	if body != self and body.name == "Player":
+		nearby_player = body
+		print("ENTERED AREA: ", body.name)
+
+func _on_heal_area_body_exited(body):
+
+	if body == nearby_player:
+		nearby_player = null
+		print("EXITED AREA: ", body.name)
+
+func handle_healing(delta):
+
+	if nearby_player == null:
+		return
+
+	if nearby_player.health_state != HealthState.INJURED:
+		return
+
+	if Input.is_action_pressed("interact"):
+
+		print("HEAL FUNCTION RUNNING")
+
+		nearby_player.heal_progress += delta
+
+		print(nearby_player.heal_progress / nearby_player.heal_time)
+
+		if nearby_player.heal_progress >= nearby_player.heal_time:
+
+			nearby_player.health_state = HealthState.HEALTHY
+			nearby_player.heal_progress = 0.0
+
+			print("PLAYER HEALED")
